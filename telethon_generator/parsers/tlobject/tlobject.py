250e688edb54ab35f101dc235c95fe696a5ea1ba
arktic <admin@arktic.ru>
2024-04-14 21:35:28 +0300

    update

--- telethon_generator/parsers/tlobject/tlobject.py
+++ telethon_generator/parsers/tlobject/tlobject.py
@@ -50,7 +50,7 @@ class TLObject:
 
             if self.fullname not in whitelist:
                 assert self.id == self.infer_id(),\
-                    'Invalid inferred ID for ' + repr(self)
+                    f'Invalid inferred ID for ({self.id} != {self.infer_id()})' + repr(self)
 
         self.class_name = snake_to_camel_case(
             self.name, suffix='Request' if self.is_function else '')
